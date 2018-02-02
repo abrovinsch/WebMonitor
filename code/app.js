7743969c4aa1eef03dc43d4c48eea0caea0836a8
Oskar Lundqvist <lundqvist.oskar01@gmail.com>
2018-02-02 17:13:28 +0100

    Navmenu working

--- code/app.js
+++ code/app.js
@@ -5,9 +5,65 @@ var app = new Vue({
   data: {
     description: 'Description',
     title: 'Title',
-    buttons: [
-      { title: 'Foo', l: 'X' },
-      { title: 'Bar', l: 'X' }
+    projects: [
+      {
+        title: 'Project 1 ',
+        open: true,
+        zones: [
+          {
+            title: 'Zone 1',
+            open: true,
+            nodes: [
+              {title: 'P4', serial: 748977}
+            ]
+          },
+          {
+            title: 'Zone 2',
+            open: true,
+            nodes: [
+              {title: 'P4', serial: 748977}
+            ]
+          },
+          {
+            title: 'Zone 3',
+            open: false,
+            nodes: [
+              {title: 'P4', serial: 748977}
+            ]
+          }
+        ]
+      },
+      {
+        title: 'Project 2',
+        open: false,
+        zones: [
+          {
+            title: 'Zone 1',
+            open: true,
+            nodes: [
+              {title: 'P4', serial: 748977}
+            ]
+          },
+          {
+            title: 'Zone 2',
+            open: true,
+            nodes: [
+              {title: 'P4', serial: 748977},
+              {title: 'P4', serial: 748977},
+              {title: 'P4', serial: 748977},
+              {title: 'P4', serial: 748977},
+              {title: 'P4', serial: 748977}
+            ]
+          },
+          {
+            title: 'Zone 3',
+            open: true,
+            nodes: [
+              {title: 'P4', serial: 748977}
+            ]
+          }
+        ]
+      }
     ]
   }
 })
@@ -25,5 +81,3 @@ function Select()
   app.title = "P4";
   app.description = "Desc";
 }
-
-document.getElementById('hide-app3').addEventListener("click", Select);
