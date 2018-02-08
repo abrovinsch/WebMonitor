8650e74181da3fa5421d9b644575cfe27b982a7d
Oskar Lundqvist <lundqvist.oskar01@gmail.com>
2018-02-08 18:22:40 +0100

    - Rewrote the way http requests and responses work

--- code/app.js
+++ code/app.js
@@ -1,10 +1,10 @@
-// WebMon 1.0
+// VUE OBJECTS
 
 var root_selection = {
-  title: "Welcome to Camur Web Monitor",
-  components: []
+  heading: "Welcome to Camur Web Monitor",
+  components: [],
+  selected: true
 }
-
 
 var inspector = new Vue({
   el: '.inspector',
@@ -15,26 +15,35 @@ var inspector = new Vue({
     ]
   },
   methods: {
-      updateLastValue: function (sel) {
-        sel.last_value = Math.random() * 0.3;
-      }
+    requestLastValue: function (sel) {
+      sel.waitingForLastValue = true;
+      sendServerRequest("last_value/" + sel.title);
+    },
+
+    requestProject :function(prj){
+      sendServerRequest("get_project/" + prj);
+    }
   }
 })
 
 var navmenu = new Vue({
   el: '.navmenu',
+
   data: {
     projects: [],
     selected: Object()
   },
+
   methods: {
     select_node: function (node, event) {
+      node.heading = node.title + " - " + node.node_type;
       inspector.title = node.title + " - " + node.node_type;
       inspector.description = node.online ? "Online" : "Offline";
       inspector.selected.selected = false;
       inspector.selected = node;
       node.selected = true;
     },
+
     select_zone: function (zone) {
       inspector.title = zone.title;
       zone.open = !zone.open;
@@ -44,76 +53,116 @@ var navmenu = new Vue({
   }
 })
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+// SERVER HANDLING
+
+function sendServerRequest(command)
+{
+  var serverDomain = "http://127.0.0.1:5000";
+  var url =  serverDomain + "/" + command;
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = receiveServerResponse;
+  httpRequest.open('GET', url, true);
+  httpRequest.send();
 }
 
-function getRandomNode()
-{
-  var _online = getRandomInt(0,3) == 1 ? false : true;
-  var _alarm = getRandomInt(0,10) == 1 ? true : false;
-  var types = ["ZRA", "P4", "WLink", "PQuick", "Fixvolt"]
-  var _type = types[Math.floor(Math.random() * types.length)];
-  var symb = 'scanner';
-  var ver = "3." + getRandomInt(3,7);
-  var _last_value = Math.random() * 0.3;
-
-  var comps = [
-    {type: "node info", open: true},
-    {type: "last values", open: true},
-  ];
-
-  if(_type == "WLink")
-  {
-    symb = 'wifi';
-    comps.push({type: "wlink", open: true})
-  }
-  if(_type == "Fixvolt")
-  {
-    symb = 'power_input';
-    comps.push({type: "fixvolt", open: true})
-  }
-
-  return {
-    title: getRandomInt(748800, 749000),
-    online: _online,
-    alarm: _alarm,
-    symbol: symb,
-    node_type: _type,
-    components: comps,
-    version: ver,
-    last_value: _last_value,
-    selected: false
-  }
-}
-
-document.title = "WebMon 1";
-
-for(var i = 0; i<3; i++)
-{
-
-  var x = {
-    title: 'Project ' + (i+1),
-    open: true,
-    zones: []
-  }
-
-  for(var j = 0; j < getRandomInt(1,4); j++)
-  {
-    var z = {
-      title: 'Zone ' + (j+1),
-      open: true,
-      nodes: []
+function receiveServerResponse() {
+  if(this.readyState === 4){
+    if (this.status === 200) {
+      handleIncomingJSONMsg(this.responseText)
+    } else {
+      // There was a problem with the request
     }
-
-    x.zones.push(z);
   }
+}
 
-  x.zones.forEach(function(zone) {
-    for(var j = 0; j < getRandomInt(1,27); j++)
+// APP
+
+function handleIncomingJSONMsg(json)
+{
+  response = JSON.parse(json);
+  if(response.response_type === "projectinfo")
+  {
+    updateProject(response.msg);
+  }
+  else if(response.response_type === "last_value"){
+    updateLastValue(response.msg);
+  }
+}
+
+function updateProject(proj)
+{
+  for (i = 0; i < proj.zones.length; i++)
+  {
+    proj.zones[i].open = true;
+    for (j = 0; j < proj.zones[i].nodes.length; j++)
     {
-      zone.nodes.push(getRandomNode());
+      var comps = [
+        {type: "node info", open: true},
+      ];
+
+      var symb = "scanner"
+      if(proj.zones[i].nodes[j].node_type == "WLink")
+      {
+        symb = 'wifi';
+        comps.push({type: "wlink", open: true})
+      }
+
+      if(proj.zones[i].nodes[j].node_type != "WLink")
+      {
+        comps.push({type: "last values", open: true})
+      }
+
+      if(proj.zones[i].nodes[j].node_type == "Fixvolt")
+      {
+        symb = 'power_input';
+        comps.push({type: "fixvolt", open: true})
+      }
+
+      proj.zones[i].nodes[j].waitingForLastValue = false;
+
+      proj.zones[i].nodes[j].components = comps;
+      proj.zones[i].nodes[j].symbol = symb;
     }
-  });
-  navmenu.projects.push(x);
+  }
+  navmenu.projects.push(proj);
 }
+
+function updateLastValue(last_val_obj)
+{
+  node = getNodeBySerial(last_val_obj.serial);
+  node.last_value = last_val_obj.value;
+  node.waitingForLastValue = false;
+}
+
+function getNodeBySerial(serial)
+{
+  //alert(serial);
+  for (i = 0; i < navmenu.projects.length; i++)
+  {
+    for (j = 0; j < navmenu.projects[i].zones.length; j++)
+    {
+      for (k = 0; k < navmenu.projects[i].zones[j].nodes.length; k++)
+      {
+        if(navmenu.projects[i].zones[j].nodes[k].serial === serial)
+        {
+          return navmenu.projects[i].zones[j].nodes[k];
+        }
+      }
+    }
+  }
+}
+
+// BUILTIN FUNCTIONS
+
+function onPageLoad()
+{
+  document.title = "WebMon 1";
+
+  for(var i = 0; i<1; i++)
+  {
+    inspector.requestProject("Project " + (i+1));
+  }
+}
+
+// START CODE
+window.onload = onPageLoad();
